825417b7506beb0dbb2e063667a20c1a5d748d84
coolshou <coolshou2060@gmail.com>
2017-10-09 03:48:49 +0800

    remove bcm2835

--- SupplySwitch/src2/softshut.c
+++ SupplySwitch/src2/softshut.c
@@ -14,8 +14,6 @@ require wiringPi
 #define SOFT_OFF_PIN 4 // PIN 16, monitor soft off pin
 #define KEEP_POWERED_PIN   5 // PIN 18, keeps power pin
 
-int prog_exit(void);
-
 int main(int argc, char **argv)
 {
     setlogmask (LOG_UPTO (LOG_NOTICE));
@@ -48,60 +46,5 @@ int main(int argc, char **argv)
     closelog ();    
     return 0;
 
-
-
-    /*
-    int relay = 0;
-    int onoff = 0;
-
- // GPIO 初始化
-    syslog(LOG_INFO, "bcm2835_init!");
-    if(!bcm2835_init()) {
-        syslog(LOG_ERR, "bcm2835 init Error!");
-        return 1;
-    }
-
- // 設定接腳狀態       
-    syslog(LOG_INFO,"set KEEP_POWERED_PIN %d output", KEEP_POWERED_PIN);
-    bcm2835_gpio_fsel(KEEP_POWERED_PIN, BCM2835_GPIO_FSEL_OUTP);
-    syslog(LOG_INFO,"set SOFT_OFF_PIN %d input", SOFT_OFF_PIN);
-    bcm2835_gpio_fsel(SOFT_OFF_PIN, BCM2835_GPIO_FSEL_INPT);
- 
-    // 初始輸出狀態
-    bcm2835_gpio_write(KEEP_POWERED_PIN, HIGH);
-    //  with a pullup
-    bcm2835_gpio_set_pud(SOFT_OFF_PIN, BCM2835_GPIO_PUD_UP);
-    // And a low detect enable
-    bcm2835_gpio_len(SOFT_OFF_PIN);
-
-    while(1)
-    {
-        if (bcm2835_gpio_eds(SOFT_OFF_PIN))
-        {   // Now clear the eds flag by setting it to 1
-            bcm2835_gpio_set_eds(SOFT_OFF_PIN);
-            syslog(LOG_INFO,"low event detect for PIN %d", SOFT_OFF_PIN);
-            syslog(LOG_INFO, "SOFT_OFF_PIN current level: %d", bcm2835_gpio_lev(SOFT_OFF_PIN));            
-        }
-        // wait a bit
-        delay(500);
-    }
-    closelog ();
-
-    return prog_exit();
-    */
 }
 
-int prog_exit()
-{
-    bcm2835_gpio_write(KEEP_POWERED_PIN, LOW); // turn off relay
-    //bcm2835_gpio_write(RELAY_INP2, HIGH);
-    
-    // 關閉 bcm2835 函式庫
-    if (!bcm2835_close())
-    {
-        printf("Failed to close the library, deallocating any allocated memory and closing /dev/mem\n");
-        printf("\nPress any key to exit...");
-        return 2;
-    }
-    return 0;
-}
