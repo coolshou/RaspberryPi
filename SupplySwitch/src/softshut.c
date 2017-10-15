6a7e420ff646303bf27dca53cf1dbb7d3ea340a7
coolshou <coolshou2060@gmail.com>
2017-10-15 09:51:27 +0800

    change from bcm2835 to wiringPi

--- SupplySwitch/src/softshut.c
+++ SupplySwitch/src/softshut.c
@@ -1,79 +1,76 @@
 /*-
     softshut.c
-    compile: gcc softshut.c -l rt -l bcm2835 -std=gnu99 -o softshut
+    compile: gcc softshut.c -l rt -l wiringPi -std=gnu99 -o softshut
 
-require bcm2835-1.52
+require wiringPi
 * 
 -*/
 
 #include <stdio.h>   // printf, scanf
-#include <bcm2835.h>
+#include <wiringPi.h>
 #include <syslog.h>
-#include <unistd.h>  //getuid
+#include <unistd.h>  //getuid()
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h> // system()
 
-#define SOFT_OFF_PIN 23 // GPIO 23 = PIN 16, monitor soft off pin
-#define KEEP_POWERED_PIN   24 // GPIO 24 = PIN 18, keeps power pin
+//
+#define SOFT_OFF_PIN 16 // wiringPi PIN 4, monitor soft off pin
+#define KEEP_POWERED_PIN   18 // wiringPi PIN 5, keeps power pin
 
-//#define RELAY_INP2  25 // GPIO #25
-
-int prog_exit(void);
+/*
+ * softoffInterrupt:
+ *********************************************************************************
+ */
+static volatile int globalCounter = 0 ;
+void softoffInterrupt (void)
+{
+  ++globalCounter ;
+  syslog(LOG_NOTICE,"softoffInterrupt value: %d", globalCounter);
+  system("halt");
+}
 
 int main(int argc, char **argv)
 {
-    int relay = 0;
-    int onoff = 0;
-    setlogmask (LOG_UPTO (LOG_NOTICE));
-    openlog ("softshut", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
-    syslog(LOG_NOTICE, "Program started by User %d", getuid ());
-    syslog(LOG_INFO, "softshut start");
+    int myCounter = 0 ;
+    setlogmask (LOG_UPTO (LOG_NOTICE)); // discard log message bacause LOG_INFO priority level is below LOG_NOTICE
+    openlog("softshut", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+    syslog(LOG_NOTICE, "Program started by User %d", getuid());
 
- // GPIO 初始化
-    syslog(LOG_INFO, "bcm2835_init!");
-    if(!bcm2835_init()) {
-        syslog(LOG_ERR, "bcm2835 init Error!");
-        return 1;
+    //wiringPiSetup: use wiringPi pin numbering scheme
+    //use the physical pin numbers on the P1 connector only.
+    if(wiringPiSetupPhys()==-1) {
+        syslog(LOG_ERR, "wiringPi Setup error");
+        return -1 ;
     }
+    syslog(LOG_NOTICE, "softshut start for Raspberry Pi revision: %d", piBoardRev());
+// 設定接腳狀態
+    syslog(LOG_NOTICE,"set KEEP_POWERED_PIN %d output", KEEP_POWERED_PIN);
+    pinMode(KEEP_POWERED_PIN,OUTPUT);
+// 初始狀態
+    digitalWrite(KEEP_POWERED_PIN, 1);
 
- // 設定接腳狀態       
-    syslog(LOG_INFO,"set KEEP_POWERED_PIN %d output", KEEP_POWERED_PIN);
-    bcm2835_gpio_fsel(KEEP_POWERED_PIN, BCM2835_GPIO_FSEL_OUTP);
-    syslog(LOG_INFO,"set SOFT_OFF_PIN %d input", SOFT_OFF_PIN);
-    bcm2835_gpio_fsel(SOFT_OFF_PIN, BCM2835_GPIO_FSEL_INPT);
- 
-    // 初始輸出狀態
-    bcm2835_gpio_write(KEEP_POWERED_PIN, HIGH);
-    //  with a pullup
-    bcm2835_gpio_set_pud(SOFT_OFF_PIN, BCM2835_GPIO_PUD_UP);
-    // And a low detect enable
-    bcm2835_gpio_len(SOFT_OFF_PIN);
-
-    while(1)
+    //syslog(LOG_NOTICE,"set SOFT_OFF_PIN %d input", SOFT_OFF_PIN);
+    //pinMode(SOFT_OFF_PIN,INPUT);
+    //TODO: not work?? no trigger!! why
+    if (wiringPiISR (SOFT_OFF_PIN, INT_EDGE_RISING, &softoffInterrupt) < 0) {
+        syslog(LOG_ERR, "Unable to setup ISR: %s\n", strerror(errno)) ;
+        return 1 ;
+    }
+    //pullUpDnControl(SOFT_OFF_PIN, PUD_UP);//http://wiringpi.com/reference/core-functions/
+    //syslog(LOG_NOTICE,"get SOFT_OFF_PIN value: %d", digitalRead(SOFT_OFF_PIN));
+    for (;;)
     {
-        if (bcm2835_gpio_eds(SOFT_OFF_PIN))
-        {   // Now clear the eds flag by setting it to 1
-            bcm2835_gpio_set_eds(SOFT_OFF_PIN);
-            syslog(LOG_INFO,"low event detect for PIN %d", SOFT_OFF_PIN);
-            syslog(LOG_INFO, "SOFT_OFF_PIN current level: %d", bcm2835_gpio_lev(SOFT_OFF_PIN));            
-        }
-        // wait a bit
-        delay(500);
+        while (myCounter == globalCounter) //wait for SOFT_OFF_PIN trigger
+            delay (100) ;
+        syslog(LOG_NOTICE, " Done. counter: %5d\n", globalCounter) ;
+        //myCounter = globalCounter ;
     }
-    closelog ();
-
-    return prog_exit();
-}
-
-int prog_exit()
-{
-    bcm2835_gpio_write(KEEP_POWERED_PIN, LOW); // turn off relay
-    //bcm2835_gpio_write(RELAY_INP2, HIGH);
+    digitalWrite(KEEP_POWERED_PIN,0);
     
-    // 關閉 bcm2835 函式庫
-    if (!bcm2835_close())
-    {
-        printf("Failed to close the library, deallocating any allocated memory and closing /dev/mem\n");
-        printf("\nPress any key to exit...");
-        return 2;
-    }
+
+    closelog ();    
     return 0;
+
 }
+
